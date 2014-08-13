59339f79692e6e562e57a5189ffb3e69c0d48a8f
slaterson <chris@tarsier.homelinux.org>
2014-08-13 15:36:18 -0700

    Changes to be committed: 	modified:   action.php
     Changes not staged for commit:
    	deleted:    action-cli.php
    	deleted:    action1.php
    	deleted:    action2.php
    	deleted:    contacts.json
    	deleted:    eloquaRequestOauth.php
    	deleted:    files/1854-download.csv
    	deleted:    oauth1.php
    	deleted:    oauth2.php

--- action.php
+++ action.php
@@ -3,7 +3,6 @@
   print "<html><head></head><body>";
   $segment = $_GET["asset"];
   print "Segment id: " . $segment . "<br>";
-#  print "Preparing your data... you might be sent an email when it's ready.";
 
   include_once ("/var/www/localhost/files/eloqua.inc");
   include_once ("eloquaRequest.php");
@@ -27,36 +26,5 @@
   print "<input type='submit' value='Export'>";
   print "</form>";
   print "</body></html>";
-
-exit;
-
-  $dictionary = (object) array('C_EmailAddress' => '{{Contact.Field(C_EmailAddress)}}',
-			       'C_FirstName' => '{{Contact.Field(C_FirstName)}}',
-			       'C_LastName' => '{{Contact.Field(C_LastName)}}',
-			       'C_Company' => '{{Contact.Field(C_Company)}}',
-			       'C_Address1' => '{{Contact.Field(C_Address1)}}',
-			       'C_Address2' => '{{Contact.Field(C_Address2)}}',
-			       'C_City' => '{{Contact.Field(C_City)}}',
-			       'C_State_Prov' => '{{Contact.Field(C_State_Prov)}}',
-			       'C_Zip_Postal' => '{{Contact.Field(C_Zip_Postal)}}',
-			       'C_Birthdate1' => '{{Contact.Field(C_Birthdate1)}}',
-			       'M_CompanyName' => '{{Contact.Account.Field(M_CompanyName)}}');
-
-  $export = (object) array('name' => 'All Contacts',
-			   'dataRetentionDuration' => 'PT1H',
-			   'filter' => "EXISTS('{{ContactSegment[" . $segment . "]}}')",
-			   'fields' => $dictionary);
-
-  $client = new EloquaRequest($eloqua_site, $eloqua_userA, $eloqua_pass, $endPointURL);
-  $response = $client->post('/contacts/exports', $export);
-  $exportUri = $response->uri;
-
-  $callbackUri = "https://mungkey.org/eloqua/menu/download.php?eu=" . urlencode($response->uri);
-
-  $sync = (object) array('callbackUrl' => $callbackUri,
-                         'syncedInstanceUri' => $response->uri);
-
-  $syncSend = $client->POST('/syncs', $sync);
-  $syncUri = $syncSend->uri;
 
 ?>
